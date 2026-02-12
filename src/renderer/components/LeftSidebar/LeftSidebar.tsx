0976685b7d473cb7b54c7ec25eb3e211a38bb9fa
Sezgin Sazlıoğulları <sezginsazlioglu@hotmail.com>
2026-02-12 23:24:41 +0300

    Remove known-bugs badge and related store usage

--- src/renderer/components/LeftSidebar/LeftSidebar.tsx
+++ src/renderer/components/LeftSidebar/LeftSidebar.tsx
@@ -1,7 +1,6 @@
 import { Bug } from 'lucide-react'
 import { useAppStore } from '../../stores/useAppStore'
 import { useRepoStore } from '../../stores/useRepoStore'
-import { useBugStore } from '../../stores/useBugStore'
 import CollectionProgress from './CollectionProgress'
 import SessionList from './SessionList'
 import ProjectContext from './ProjectContext'
@@ -10,10 +9,8 @@ import QuickActions from './QuickActions'
 export default function LeftSidebar() {
   const { activeView, toggleBugView, activeTab, setActiveView } = useAppStore()
   const { getRepoByName } = useRepoStore()
-  const bugs = useBugStore((s) => s.bugs)
 
   const activeRepo = activeTab ? getRepoByName(activeTab) : null
-  const openBugCount = bugs.filter(b => b.status === 'open').length
 
   const handleSessionClick = () => {
     if (activeView === 'bugs') {
@@ -35,9 +32,6 @@ export default function LeftSidebar() {
         >
           <Bug size={16} />
           <span>Known Bugs</span>
-          {openBugCount > 0 && (
-            <span className="known-bugs-btn__badge">{openBugCount}</span>
-          )}
         </button>
       </div>
       <ProjectContext />
